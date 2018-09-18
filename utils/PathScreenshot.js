2f971f4855c9d4e8dd36236c01beebcf88993c69
Binh Nguyen <blcsntb@gmail.com>
2018-09-18 13:09:19 +0700

    Add function remove white space

--- utils/PathScreenshot.js
+++ utils/PathScreenshot.js
@@ -1,7 +1,11 @@
 
 const pathScreenshot = ({ platformName, deviceName, platformVersion }) => (id) => {
   const path = `./screenshots/${id}-${platformName}-${deviceName}-${platformVersion}.png`;
-  return path.replace(' ', '_')
+  return removeWhiteSpace(path);
 };
+
+export function removeWhiteSpace(str) {
+  return str.replace(/ /g, '');
+}
 
 export default pathScreenshot;
