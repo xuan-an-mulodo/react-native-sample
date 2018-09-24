e01fc2bed9aa80ce9643b7b3c2284aabef0da0d2
nguyen-xuan-an-mulodo <41279153+nguyen-xuan-an-mulodo@users.noreply.github.com>
2018-09-24 13:59:15 +0700

    Add FLAG_SECURE
    Adding FLAG_SECURE for capture screenshot

--- android/app/src/main/java/com/breactnative/MainActivity.java
+++ android/app/src/main/java/com/breactnative/MainActivity.java
@@ -1,8 +1,18 @@
 package com.breactnative;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.support.annotation.Nullable;
 
 public class MainActivity extends ReactActivity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                WindowManager.LayoutParams.FLAG_SECURE);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
